8d4f8a91239b9a543444abbdafa35c36fe32fa9a
nshams <nehalshams786@gmail.com>
2023-07-13 12:06:41 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Layout from './layout'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <Layout>
+        
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/layout/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResponsiveTopBar from './Topbar'
+
+const Layout = () => {
+  return (
+    <div>
+        <ResponsiveTopBar/>
+    </div>
+  )
+}
+
+export default Layout
